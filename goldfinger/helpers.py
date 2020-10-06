3550fcd42fea7501029f9117b6983ab24600d527
Ryan Tracey <ryan.tracey@mobilityhouse.com>
2020-10-07 00:17:08 +0200

    First "working" version definition of "working"... o_O

--- goldfinger/helpers.py
+++ goldfinger/helpers.py
@@ -3,6 +3,8 @@ Helper functions
 """
 
 import json
+from datetime import date, datetime, timedelta
+
 
 def ounce_to_grams(ounces):
     return ounces * 28.3495
@@ -10,3 +12,19 @@ def ounce_to_grams(ounces):
 
 def pretty_print_json(json_dict):
     print(json.dumps(json_dict, indent=2))
+
+
+def get_today_date():
+    return date.today().strftime('%Y-%m-%d')
+
+
+def get_days_ago_date(days_ago):
+    d = datetime.now() - timedelta(days_ago)
+    return d.strftime('%Y-%m-%d')
+
+
+def days_diff(start_date, end_date):
+    start = datetime.strptime(start_date, '%Y-%m-%d')
+    end = datetime.strptime(end_date, '%Y-%m-%d')
+    diff = end - start
+    return diff.days
--- goldfinger/metals_api.py
+++ goldfinger/metals_api.py
@@ -4,16 +4,24 @@ For example, for 1 USD the return is a number like 0.000634 for Gold (XAU).
 To get the gold rate in USD: 1/0.000634= 1577.28 USD
 """
 
+import math
 import boto3
+import redis
 import requests
+import pandas as pd
 
+from datetime import date, datetime, timedelta
+from helpers import get_today_date, get_days_ago_date, days_diff
+
+MAX_DAYS = 5
 API_URL = 'https://www.metals-api.com/api'
 
 ssm = boto3.client('ssm')
 access_key = ssm.get_parameter(Name='/goldfinger/api/key', WithDecryption=True)['Parameter']['Value']
 
+r = redis.Redis()
 
-def get_latest(currency='ZAR', *symbols):
+def get_latest(currency, *symbols):
     """
     "latest" endpoint - request the most recent exchange rate data
 
@@ -25,11 +33,24 @@ def get_latest(currency='ZAR', *symbols):
     """
     symbols = ','.join(symbols)
     uri = f'{API_URL}/latest?access_key={access_key}&base={currency}&symbols={symbols}'
-    print(uri)
-    ret = requests.get(uri)
-    print(ret.status_code)
-    print(ret.text)
-    print(ret.json())
+    return requests.get(uri).json()
+
+
+def get_timeseries(currency, start_date, end_date, symbol):
+    """
+    "timeseries" endpoint - request exchange rates for a specific period of time
+
+    https://www.metals-api.com/api/timeseries
+
+      ? access_key = YOUR_ACCESS_KEY
+      & start_date = YYYY-MM-DD
+      & end_date = YYYY-MM-DD
+      & base = USD
+      & symbols = XAU,XAG <-- can actually only be one symbol
+    """
+    uri = f'{API_URL}/timeseries?access_key={access_key}&start_date={start_date}&end_date={end_date}&base={currency}&symbols={symbol}'
+    return requests.get(uri).json()
+
 
 def get_historical():
     """
@@ -63,19 +84,6 @@ def get_convert():
     pass
 
 
-def get_timeseries():
-    """
-    "timeseries" endpoint - request exchange rates for a specific period of time
-
-    https://www.metals-api.com/api/timeseries
-
-      ? access_key = YOUR_ACCESS_KEY
-      & start_date = YYYY-MM-DD
-      & end_date = YYYY-MM-DD
-      & base = USD
-      & symbols = XAU,XAG
-    """
-    pass
 
 def get_fluctuation():
     """
@@ -95,6 +103,76 @@ def get_fluctuation():
     """
     pass
 
+
+def redis_to_dataframe(symbol):
+    timeseries_data = r.hgetall(symbol)
+    timeseries_data = {
+        k.decode('utf-8'):float(v) for (k,v) in timeseries_data.items()
+    }
+    df = pd.DataFrame(timeseries_data.items(), columns=['Date', 'DateValue'])
+    pd.to_datetime(df['Date'])
+    return df
+
+
+def timeseries_to_redis(currency, start_date_str, end_date_str, symbol):
+    today = datetime.today()
+    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+    _days_diff = days_diff(start_date_str, end_date_str)
+    loops = math.ceil(_days_diff / MAX_DAYS)
+    rates = {}
+    for loop in range(loops):
+        start = start_date
+        end = start_date + timedelta(MAX_DAYS)
+        if end > today:
+            end = today
+        end_str =  end.strftime('%Y-%m-%d')
+        start_str = start.strftime('%Y-%m-%d')
+        print(f'{start_str} to {end_str}')
+        # this does a hget each iteration, but I guess that's what a cache is for
+        if not date_range_in_redis(start, symbol):
+            # redis does not have the keys in range
+            ret = get_timeseries(currency, start_str, end_str, symbol)
+        else:
+            print('...already in redis')
+            continue
+        if not ret['success']:
+            print(ret)
+            break
+        rates.update(ret['rates'])
+        start_date = end
+    # flatten dictionary
+    rates_to_date = {}
+    rates_to_date = {
+        k:v[symbol] for (k,v) in rates.items()
+    }
+    return {symbol: rates_to_date}
+
+#end_date = get_today_date()
+#start_date = get_days_ago_date(MAX_DAYS)
+
+def date_range_in_redis(start_date, symbol):
+    timeseries_data = r.hgetall(symbol)
+    timeseries_data = {
+        k.decode('utf-8'):float(v) for (k,v) in timeseries_data.items()
+    }
+    all_dates = set(timeseries_data.keys())
+    range_dates =  set([(start_date + timedelta(days=x)).strftime('%Y-%m-%d') for x in range(MAX_DAYS)])
+    return range_dates.issubset(all_dates)
+
+
 if __name__=="__main__":
 
-    get_latest('ZAR', 'XAU', 'XAG')
+    yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+
+    for symbol in ['XAU', 'XAG']:
+        series = timeseries_to_redis('ZAR', '2020-01-01', '2020-10-04', symbol)
+        print(series)
+        if series:
+            try:
+                r.hmset(symbol, series[symbol])
+                print(redis_to_dataframe(symbol))
+            except redis.exceptions.DataError:
+                print('empty dictionary')
+        else:
+            print('Something went wrong')
